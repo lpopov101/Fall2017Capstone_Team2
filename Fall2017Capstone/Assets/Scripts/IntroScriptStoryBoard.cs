9acc3439293fc0fd78d61a0e5010a17bbdee7f2b
GohJazn <jaemin_goh@yahoo.com>
2017-09-21 13:07:54 -0500

    Honestly, I forgot

---
+++ Fall2017Capstone/Assets/Scripts/IntroScriptStoryBoard.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IntroScriptStoryBoard : MonoBehaviour {
+
+	public Text text;
+	public Text pressE;
+
+	// Use this for initialization
+	void Start () {
+		text.enabled = false;
+		//pressE.enabled = false;
+		StartCoroutine (WaitForAudio());
+	}
+
+	IEnumerator WaitForAudio() {
+		yield return new WaitForSeconds (17.0f);
+		text.enabled = true;
+		//pressE.enabled = true;
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
