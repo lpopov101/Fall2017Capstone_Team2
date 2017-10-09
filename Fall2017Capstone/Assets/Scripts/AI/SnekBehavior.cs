3efe7f5f18bf55bb8c149bc39b8b3a95edb2ac20
Scott <scott.huang.21@outlook.com>
2017-10-08 22:06:27 -0500

    Tweak dodge ability; fix up snek sometimes not moving

--- Fall2017Capstone/Assets/Scripts/AI/SnekBehavior.cs
+++ Fall2017Capstone/Assets/Scripts/AI/SnekBehavior.cs
@@ -5,29 +5,26 @@ using UnityEngine;
 public class SnekBehavior : MonoBehaviour {
 
 	public float movementSpeed = 1.0f;
-	public float distanceBeforeFollowing = 3.0f;
+	public float minFollowDistance;
+	public float maxFollowDistance;
 
 	private Rigidbody2D rigidBody;
-	private Vector3 initialPosition;
 	private GameObject player;
 	private PlayerControllerImproved playerController;
-	private bool playerOnRightInitial;
 	private bool facingRight;
 
 	void Start () {
 		rigidBody = GetComponent<Rigidbody2D>();
-		initialPosition = transform.position;
 		player = GameObject.FindGameObjectWithTag("Player");
 		playerController = player.GetComponent<PlayerControllerImproved>();
 
-		playerOnRightInitial = player.transform.position.x > transform.position.x;
 		facingRight = true;
 	}
 
 	void Update() {
+		// Set the direction the snek is facing
 		float scaleX = Mathf.Abs(transform.localScale.x);
 		scaleX *= facingRight ? 1 : -1;
-
 		transform.localScale = new Vector3(scaleX, transform.localScale.y, 1);
 	}
 
@@ -35,15 +32,7 @@ public class SnekBehavior : MonoBehaviour {
 		float dist = Vector2.Distance(player.transform.position, transform.position);
 		bool playerOnRight = player.transform.position.x > transform.position.x;
 		bool playerFacingRight = playerController.GetFacingRight();
-		bool initialOnRight = transform.position.x >= initialPosition.x; 
-		bool initialOnLeft  = transform.position.x <= initialPosition.x;
-		bool followPlayer = dist > distanceBeforeFollowing;
-
-		// Make sure the snek isn't going the wrong way past its initial position
-		if(playerOnRightInitial && initialOnRight && playerOnRight)
-			followPlayer = false;
-		if(!playerOnRightInitial && initialOnLeft && !playerOnRight)
-			followPlayer = false;
+		bool followPlayer = dist > minFollowDistance && dist < maxFollowDistance;
 		
 		// Make sure the player isn't looking at the snek
 		if(playerFacingRight && !playerOnRight)
@@ -51,8 +40,8 @@ public class SnekBehavior : MonoBehaviour {
 		if(!playerFacingRight && playerOnRight)
 			followPlayer = false;
 
+		// Move
 		if(followPlayer) {
-			// Move
 			float x = playerOnRight ? movementSpeed : -movementSpeed;
 			float smoothX = Mathf.Lerp(rigidBody.velocity.x, x, 1.0f);
 
--- Fall2017Capstone/Assets/Scripts/Player/DodgeScript.cs
+++ Fall2017Capstone/Assets/Scripts/Player/DodgeScript.cs
@@ -5,37 +5,60 @@ using UnityEngine.UI;
 
 public class DodgeScript : MonoBehaviour {
 
-
-	public bool gotDodge;
-	Rigidbody2D rigidBody;
 	public float dodgeForce = 200.0f;
+	public float dodgeTime = 0.25f;
+	public float cooldownTime = 1f;
 
-	// Use this for initialization
+	private Rigidbody2D rigidBody;
+	private PlayerControllerImproved playerController;
+	private bool hasDodgeAbility;
+	private bool gotDodge;
+	private Vector2 preDodgeVelocity;
+
 	void Start () {
-		gotDodge = false;
 		rigidBody = GetComponent<Rigidbody2D>();
+		playerController = GetComponent<PlayerControllerImproved>();
+		hasDodgeAbility = false;
+		gotDodge = true;
+		preDodgeVelocity = Vector2.zero;
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-		if (gotDodge) {
-			float horizontalAxis = Input.GetAxisRaw("Horizontal");
+		if (hasDodgeAbility && gotDodge && playerController.CanUseAbilities()) {
 			if (Input.GetButtonDown("Left Control")) {
-				
-				Vector2 vector = new Vector2 (horizontalAxis * dodgeForce,0);
-				Debug.Log ("Applying Dodge + "+ vector);
-				rigidBody.AddForce(vector,ForceMode2D.Force);
-				//transform.position = Vector3.Lerp(transform.position.x + 10f,transform.position.y,Time.deltaTime*2.0f);
-				StartCoroutine (CoolDown());
+				ApplyDodgeForce();
+
+				StartCoroutine (DodgeAndCoolDown());
 			}
 		}
 	}
 
-	IEnumerator CoolDown() {
-		Debug.Log ("CoolDown");
+	private void ApplyDodgeForce() {
+		preDodgeVelocity = rigidBody.velocity;
+
+		float horizontalAxis = Input.GetAxisRaw("Horizontal");
+		Vector2 vector = new Vector2 (horizontalAxis * dodgeForce,0);
+		rigidBody.AddForce(vector, ForceMode2D.Force);
+
+		//transform.position = Vector3.Lerp(transform.position.x + 10f,transform.position.y,Time.deltaTime*2.0f);
+	}
+
+	IEnumerator DodgeAndCoolDown() {
 		gotDodge = false;
-		yield return new WaitForSeconds(1.5f);
+
+		yield return new WaitForSeconds(dodgeTime);
+		Vector3 vel = preDodgeVelocity;
+		float sign = rigidBody.velocity.x > 0 ? 1 : -1;
+		vel.x = Mathf.Abs(preDodgeVelocity.x) * sign;
+		vel.y = Mathf.Min(vel.y, 0);
+		rigidBody.velocity = vel;
+
+		yield return new WaitForSeconds(cooldownTime);
+
 		gotDodge = true;
-		Debug.Log ("CoolDown complete");
+	}
+
+	public void SetHasDodge(bool has) {
+		hasDodgeAbility = has;
 	}
 }
