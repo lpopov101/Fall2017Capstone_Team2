d3a4b42cbbe791d87cc4a120e7208c5416866091
GohJazn <jaemin_goh@yahoo.com>
2017-10-10 17:08:48 -0500

    add monster sounds, gatorp sounds, boss attack sounds, parallax, and some additional environment assets.

---
+++ Fall2017Capstone/Assets/Scripts/BossSound.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossSound : MonoBehaviour {
+
+	public AudioClip crackAudio;
+
+	void CrackSound() {
+		AudioSource.PlayClipAtPoint(crackAudio, Camera.main.transform.position, 0.2f);
+	}
+}
---
+++ Fall2017Capstone/Assets/Scripts/Parallax.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour {
+
+	public float parallaxSpeed;
+
+	private float lastCameraX;
+	private Transform cameraTransform;
+
+	// Use this for initialization
+	void Start () {
+		cameraTransform = Camera.main.transform;
+		lastCameraX = cameraTransform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float deltaX = cameraTransform.position.x - lastCameraX;
+		transform.position -= Vector3.right * (deltaX * parallaxSpeed);
+		lastCameraX = cameraTransform.position.x;
+	}
+}
