f2237796e4d148bf28767b3e6019c8444163f284
GohJazn <jaemin_goh@yahoo.com>
2017-11-05 22:28:43 -0600

    add shell3, add core

---
+++ Fall2017Capstone/Assets/Scripts/CoreScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoreScript : MonoBehaviour {
+
+	private CameraScript cameraScript;
+	public float cameraSize;
+	public Vector2 cameraOffset;
+
+	// Use this for initialization
+	void Start () {
+		cameraScript = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraScript>();
+		cameraScript.SetCameraViewport(cameraSize, cameraOffset);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
