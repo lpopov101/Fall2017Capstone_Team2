dabdb0a7930b3891e2437724dc8fb059dba2533b
Scott <scott.huang.21@outlook.com>
2017-11-12 18:32:50 -0600

    Add toxic ground slowdown in shell2

---
+++ Fall2017Capstone/Assets/Scripts/SlowdownBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowdownBehavior : MonoBehaviour {
+
+	public float slowdownMultiplier;
+
+	private Rigidbody2D playerRb;
+
+	void Start () {
+		playerRb = null;
+	}
+
+	void Update () {
+		if(playerRb) {
+			Vector2 velocity = playerRb.velocity;
+			velocity.x *= slowdownMultiplier;
+			playerRb.velocity = velocity;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if(coll.gameObject.CompareTag("Player")) {
+			playerRb = coll.gameObject.GetComponent<Rigidbody2D>();
+		}
+	}
+
+	void OnCollisionExit2D(Collision2D coll) {
+		if(coll.gameObject.CompareTag("Player")) {
+			playerRb = null;
+		}
+	}
+}
