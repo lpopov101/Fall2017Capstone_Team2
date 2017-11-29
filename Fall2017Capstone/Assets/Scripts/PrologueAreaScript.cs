3361b79948d55c1d659150a5b2ff45290ee8c1a9
lpopov101 <you@example.com>
2017-11-29 00:38:58 -0600

    Created prologue level and fixed beginning of Shell 1

---
+++ Fall2017Capstone/Assets/Scripts/PrologueAreaScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrologueAreaScript : MonoBehaviour {
+    public CameraScript cameraScript;
+    public float cameraSize;
+    public Vector2 cameraOffset;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            cameraScript.SetCameraViewport(cameraSize, cameraOffset);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            cameraScript.ResetCameraViewport();
+        }
+    }
+}
