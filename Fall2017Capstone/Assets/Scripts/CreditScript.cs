a3e1f955fa093a04e0aa25489e42f437ed43818a
Scott <scott.huang.21@outlook.com>
2017-12-05 12:20:23 -0600

    Polish Core; minor fixes to shell 3 boss

--- Fall2017Capstone/Assets/Scripts/CreditScript.cs
+++ Fall2017Capstone/Assets/Scripts/CreditScript.cs
@@ -5,10 +5,33 @@ using UnityEngine;
 public class CreditScript : MonoBehaviour {
 
 	public Animator anim;
+	public float creditsDuration;
+	public GameObject mainMenuButton;
+
+	private bool playedCreditsAlready;
+	private float startCreditsTime;
+	private bool showedMainMenuButton;
+
+	void Start() {
+		playedCreditsAlready = false;
+		showedMainMenuButton = false;
+		mainMenuButton.SetActive(false);
+	}
+
+	void Update() {
+		float bufferTime = 2;
+		if(playedCreditsAlready && !showedMainMenuButton && Time.time > startCreditsTime + creditsDuration) {
+			showedMainMenuButton = true;
+			anim.SetBool("scroll", false);
+			mainMenuButton.SetActive(true);
+		}
+	}
 
 	void OnTriggerEnter2D(Collider2D coll) {
-		if (coll.gameObject.CompareTag ("Player")) {
+		if (!playedCreditsAlready && coll.gameObject.CompareTag ("Player")) {
 			Debug.Log ("Start Credz");
+			startCreditsTime = Time.time;
+			playedCreditsAlready = true;
 			anim.SetBool ("scroll",true);
 		}
 	}
