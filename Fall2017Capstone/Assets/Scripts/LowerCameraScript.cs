bcf374537eb2eccd93c5a9ed3e486832da85ad99
Scott <scott.huang.21@outlook.com>
2018-03-05 20:00:40 -0600

    Add to newer shell 1

---
+++ Fall2017Capstone/Assets/Scripts/LowerCameraScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LowerCameraScript : MonoBehaviour, IDimensionEventListener {
+
+	public static bool ignoreExitTrigger = false;
+
+	public Vector2 offset;
+	private CameraScript camScript;
+
+	void Start () {
+		camScript = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraScript>();
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if(other.gameObject.CompareTag("Player")) {
+			camScript.SetCameraOffset(offset);
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		if(other.gameObject.CompareTag("Player")) {
+			if(!ignoreExitTrigger)
+				camScript.ResetCameraViewport();
+			else
+				ignoreExitTrigger = false;
+		}
+	}
+
+	public void OnDimensionChange(bool reality) {
+		ignoreExitTrigger = true;
+	}
+}
